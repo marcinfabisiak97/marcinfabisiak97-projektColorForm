1224b7b4c3cd559d2295ec55ca30285a8f7f147b
marcinfabisiak97 <marcinfab1@o2.pl>
2022-04-15 01:33:22 +0200

    logic chandges to color rectangle

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import './App.scss';
 import Formpage from './pages/formPage'
 const App = () => {
--- src/components/firstForm/index.js
+++ src/components/firstForm/index.js
@@ -1,102 +1,122 @@
 import React, { useState, useEffect } from "react";
-import { colorList } from "../../utils/const";
+import { PREDEFINED_COLORS } from "../../utils/const";
+import { RGBToSaturation } from "../../utils/helpers";
+import { hexToRgb, RGBToHex } from "../../utils/helpers";
 const getDatafromLS = () => {
-    const data = localStorage.getItem('colors');
+    const data = localStorage.getItem("colors");
     if (data) {
         return JSON.parse(data);
+    } else {
+        return [];
     }
-    else {
-        return []
-    }
-}
+};
 const Firstform = () => {
     const [formValues, setFormValues] = useState({ text: "" });
     const [formErrors, setFormErrors] = useState({});
-    const [opt, setOpt] = useState('list')
-    const [ColorList, setColorList] = useState(colorList)
+    const [opt, setOpt] = useState("list");
+    const [colorList, setColorList] = useState(PREDEFINED_COLORS);
     // input field states
-    const [title, setTitle] = useState('');
-    ColorList.sort((a, b) => {
-        if (b.red === a.red) {
-            if (b.green === a.green) {
-                return b.blue - a.blue
-            } return b.green - a.green
-        }
-        return b.red - a.red
-    })
+    const [title, setTitle] = useState("");
     //input change handler
     const handleChange = (e) => {
         const { name, value } = e.target;
         setFormValues({ ...formValues, [name]: value });
-        setTitle(e.target.value)
+        setTitle(e.target.value);
     };
     //form submission handler
     const onSubmit = (e) => {
         e.preventDefault();
         // setFormErrors(validate(formValues));
         if (validate(formValues)) {
-            console.log('zwalidowany', formValues.text)
-            const [red, green, blue] = formValues.text.split(',')
+            // console.log("zwalidowany", formValues.text);
+            const [red, green, blue] = formValues.text.split(",");
+            const isHex = formValues.text.includes("#");
             let color = {
                 id: new Date().getTime().toString(),
                 title,
-                red,
-                green,
-                blue,
                 isRemovable: true,
+                isHex,
+            };
+            if (isHex) {
+                color.nameHex = formValues.text;
+                console.log(hexToRgb(color.nameHex))
+                const { red, green, blue } = hexToRgb(formValues.text)
+                color.red = red;
+                color.green = green;
+                color.blue = blue;
+            } else {
+                color.red = red;
+                color.green = green;
+                color.blue = blue;
+                color.nameHex = RGBToHex(color.red, color.red, color.red)
+                console.log(RGBToHex(123, 123, 123))
             }
-            console.log([...ColorList, color], '[...ColorList, color]')
-            const newColorList = [...ColorList, color]
-            const newUserColorList = newColorList.filter((color) => color.isRemovable)
+            // console.log([...colorList, color], "[...colorList, color]");
+            const newColorList = [...colorList, color];
+            const newUserColorList = newColorList.filter(
+                (color) => color.isRemovable
+            );
             // setColors([...colors, color]);
-            setColorList(newColorList)
-            localStorage.setItem('colors', JSON.stringify(newUserColorList));
-            setTitle('');
+            setColorList(newColorList);
+            localStorage.setItem("colors", JSON.stringify(newUserColorList));
+            setTitle("");
         }
     };
     useEffect(() => {
-        setColorList([...ColorList, ...getDatafromLS()])
-    }, [])
+        setColorList([...colorList, ...getDatafromLS()]);
+    }, []);
     //form validation handler
     const validate = (values) => {
         let errors = {};
-        const regex = /^#+([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$|^(rgb)?\(?([01]?\d\d?|2[0-4]\d|25[0-5])(\W+)([01]?\d\d?|2[0-4]\d|25[0-5])\W+(([01]?\d\d?|2[0-4]\d|25[0-5])\)?)$/i;
+        const regex =
+            /^#+([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$|^(rgb)?\(?([01]?\d\d?|2[0-4]\d|25[0-5])(\W+)([01]?\d\d?|2[0-4]\d|25[0-5])\W+(([01]?\d\d?|2[0-4]\d|25[0-5])\)?)$/i;
         if (!values.text) {
             errors.text = "Cannot be blank";
         } else if (!regex.test(values.text)) {
-            errors.text = "Only RGB format example: 125,125,125 or HEX format exampe #001dab";
+            errors.text =
+                "Only RGB format example: 125,125,125 or HEX format exampe #001dab";
         } else if (regex.test(values.text)) {
-            return true
-        } setFormErrors(errors);
-        return false
+            return true;
+        }
+        setFormErrors(errors);
+        return false;
     };
 
     // delete color from LS
     const deleteColor = (id) => {
-        const filteredColors = ColorList.filter((element, index) => {
-            return element.id !== id
-        })
-        const newUserColorList = filteredColors.filter((color) => color.isRemovable)
+        const filteredColors = colorList.filter((element, index) => {
+            return element.id !== id;
+        });
+        const newUserColorList = filteredColors.filter(
+            (color) => color.isRemovable
+        );
         setColorList(filteredColors);
-        localStorage.setItem('colors', JSON.stringify(newUserColorList));
-    }
-    console.log(colorList)
+        localStorage.setItem("colors", JSON.stringify(newUserColorList));
+    };
+
     useEffect(() => {
-        const kolor = getComputedStyle(document.documentElement).getPropertyValue('--accent-color')
-        document.documentElement.style.setProperty('--accent-color', kolor)
-        var elem = document.querySelectorAll('.rectangle17');
+        // const kolor = getComputedStyle(document.documentElement).getPropertyValue('--accent-color')
+        // document.documentElement.style.setProperty('--accent-color', kolor)
+
+        var elem = document.querySelectorAll(".rectangle");
         for (let i = 0; i < elem.length; i++) {
-            elem[i].style.backgroundColor = "red";
+            const isHex = elem[i].getAttribute("data-isHex") === "true";
+            //console.log(elem[i].getAttribute("data-color"), "datacolor");
+            // if (isHex) {
+            //     elem[i].style.backgroundColor = elem[i].getAttribute('data-color');
+            // }
+
+            elem[i].style.backgroundColor = elem[i].getAttribute("data-color");
         }
-    }, [])
+    }, [colorList, colorList.length]);
     const putColor = (kolor) => {
-        document.documentElement.style.setProperty('--accent-color', kolor)
-    }
+        document.documentElement.style.setProperty("--accent-color", kolor);
+    };
     return (
         <div>
-            <div >
+            <div>
                 <form onSubmit={onSubmit}>
-                    <div >
+                    <div>
                         <label htmlFor="text">color</label>
                         <input
                             type="text"
@@ -112,37 +132,62 @@ const Firstform = () => {
                 </form>
             </div>
             <div>
-                <label >color</label>
+                <label>color</label>
                 <select value={opt} onChange={(e) => setOpt(e.target.value)}>
-                    <option value='list'> all</option>
-                    <option value='red50'> red > 127</option>
-                    <option value='green50'> green > 127</option>
-                    <option value='blue50'> blue > 127</option>
+                    <option value="list"> all</option>
+                    <option value="red50"> red > 127</option>
+                    <option value="green50"> green > 127</option>
+                    <option value="blue50"> blue > 127</option>
+                    <option value="Sat50"> Saturation > 50%</option>
                 </select>
-                List length: {ColorList.length}
-                {
-                    ColorList.filter((color) => {
-                        if (opt === 'list') return true
-                        else if (opt === 'red50') {
-                            return color.red > 127
+                List length: {colorList.length}
+                {colorList
+                    .filter((color) => {
+                        if (opt === "list") return true;
+                        else if (opt === "red50") {
+                            return color.red > 127;
+                        } else if (opt === "green50") {
+                            return color.green > 127;
+                        } else if (opt === "blue50") {
+                            return color.blue > 127;
+                        } else if (opt === "Sat50") {
+                            const saturation = RGBToSaturation(
+                                color.red,
+                                color.green,
+                                color.blue
+                            );
+                            return saturation > 50;
+                        } return true
+                    })
+                    .sort((a, b) => {
+                        if (b.red === a.red) {
+                            if (b.green === a.green) {
+                                return b.blue - a.blue;
+                            }
+                            return b.green - a.green;
                         }
-                        else if (opt === 'green50') {
-                            return color.green > 127
-                        }
-                        else if (opt === 'blue50') {
-                            return color.blue > 127
-                        }
-                    }).map((color, index) => {
+                        return b.red - a.red;
+                    })
+                    .map((color, index) => {
                         return (
                             <div key={index}>
-                                <div data-color={color.title} className={color.class}></div>
-                                HEX:{color.nameHex}, RGB({color.red}, {color.green}, {color.blue})
-                                {color.isRemovable && <button onClick={() => deleteColor(color.id)}>delete</button>}
-                            </div >
-                        )
-                    })
-                }
-                <div >
+                                <div
+                                    data-color={
+                                        color.isHex
+                                            ? color.nameHex
+                                            : `rgb(${color.red},${color.green},${color.blue})`
+                                    }
+                                    className="rectangle"
+                                ></div>
+                                HEX:{color.nameHex}, RGB({color.red}, {color.green},{" "}
+                                {color.blue})
+                                {color.isRemovable && (
+                                    <button onClick={() => deleteColor(color.id)}>delete</button>
+                                )}
+                            </div>
+                        );
+                    })}
+                <div>
                     {/* {colorList.length > 0 && <>
                         <div >
                             {colorList.map((color, colorIndex) => (
@@ -159,8 +204,7 @@ const Firstform = () => {
                     </>} */}
                 </div>
             </div>
-
-        </div >
+        </div>
     );
-}
+};
 export default Firstform;
